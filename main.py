2dfea0680aeef1c8433750382773c6a5af7ce518
jbena4 <james.benadum@gmail.com>
2022-02-20 17:25:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -86,34 +86,33 @@ while True:
     # code to generate signal
     rsi_sell = (rsi_close>70) & (rsi_close.shift(1)<=70)
     rsi_buy = (rsi_close<30) & (rsi_close.shift(1)>=30)
+    
+    rsi_signals=pd.concat([rsi_buy.iloc[-1],rsi_sell.iloc[-1]],axis=1)
+    rsi_signals.columns=['RSI Buy', 'RSI Sell']
+
 
     # calculate macd for dataframe
     ma_short = close_df.ewm(span=12, adjust=False).mean()
     ma_long = close_df.ewm(span=26, adjust=False).mean()
-    macd = ma_short - ma_long
 
-    # code to generate signal
-    ma_sell = ((ma_short <= ma_long) & (ma_short.shift(1) >= ma_long.shift(1)))
-    ma_buy = ((ma_short >= ma_long) & (ma_short.shift(1) <= ma_long.shift(1)))
 
-    # code for SMS
-    carriers = {
-        ‘att’:    ‘@mms.att.net’,
-        ‘tmobile’:' @tmomail.net’,
-        ‘verizon’:  ‘@vtext.com’,
-        ‘sprint’:   ‘@page.nextel.com’
-    }
-    def send(message):
-            # Replace the number with your own, or consider using an argument\dict for multiple people.
-        to_number = ’000-000-0000{}’.format(carriers[‘att’])
-        auth = (’**email**’, ’**password**’)
+    ma_signals=pd.concat([ma_buy.iloc[-1], ma_sell.iloc[-1]], axis=1)
+    ma_signals.columns=['MA Buy','MA Sell']
+    all_signals=pd.concat([rsi_signals,ma_signals],axis=1)
+   
+
+def send(message):
+        # Replace the number with your own @ carrier and gmail address and password. *Not secure*
+        to_number = '1234567890@vtext.com'
+        auth = ('email@gmail.com','password')
         # Establish a secure session with gmail’s outgoing SMTP server using your gmail account
-        server = smtplib.SMTP( “smtp.gmail.com”, 587 )
+        server = smtplib.SMTP('smtp.gmail.com', 587 )
         server.starttls()
         server.login(auth[0], auth[1])
         # Send text message through SMS gateway of destination number
         server.sendmail( auth[0], to_number, message)
-    I generally call the script SMS.py. Then use it in the following way.
-    import SMS
-    some_text = ‘Blah, blah’
-    SMS.send(some_text)
+
+
+if True in all_signals.values:
+    some_text = f'you have a stock signal {all_signals}'
+    send(some_text)
